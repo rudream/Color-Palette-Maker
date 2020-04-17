2d7020aa11cef80987be3ea39fdfe3fe0ece7cf6
rudream12 <rudream12@gmail.com>
2020-04-17 13:09:56 -0400

    added and styled sliders

--- app.js
+++ app.js
@@ -22,5 +22,39 @@ function randomColors() {
         const randomColor = generateHex();
         div.style.backgroundColor = randomColor;
         hexText.innerText = randomColor;
+        updateContrast(randomColor, hexText); //Updates the color of the text based on the color to increase contrast and make it readable
+        updateContrast(randomColor, div.children[1].children[0]); //Also updates the color of the buttons
+        updateContrast(randomColor, div.children[1].children[1]);
+        const color = chroma(randomColor);
+        const sliders = div.querySelectorAll(".sliders input");
+        const brightness = sliders[0];
+        const hue = sliders[1];
+        const saturation = sliders[2];
+        colorizeSliders(color, hue, brightness, saturation);
     });
+}
+
+randomColors();
+
+function updateContrast(color, text) {
+    const luminance = chroma(color).luminance();
+    if (luminance > 0.5) {
+        text.style.color = "black";
+    } else {
+        text.style.color = "white";
+    }
+}
+
+function colorizeSliders(color, hue, brightness, saturation) {
+    //Hue Scale
+    hue.style.backgroundImage = `linear-gradient(to right, rgb(204, 75, 75), rgb(204, 204, 75), rgb(75, 204, 75), rgb(75, 204, 204), rgb(75, 75, 204), rgb(204, 75, 204), rgb(204, 75, 75)`;
+    //Brightness Scale
+    const midBright = color.set("hsl.l", 0.5);
+    const scaleBright = chroma.scale(["black", midBright, "white"]);
+    brightness.style.backgroundImage = `linear-gradient(to right, ${scaleBright(0)}, ${scaleBright(1)})`; //prettier-ignore
+    //Saturation Scale
+    const noSat = color.set("hsl.s", 0);
+    const fullSat = color.set("hsl.s", 1);
+    const scaleSat = chroma.scale([noSat, color, fullSat]);
+    saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSat(0)}, ${scaleSat(1)})`; //prettier-ignore
 }
